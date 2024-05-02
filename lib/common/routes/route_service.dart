0e0155c50ff513b3b78c0c17996274bee63ef77f
senenpalanca97 <senen@trialing.org>
2024-05-02 09:40:18 +0200

    First Commit

---
+++ lib/common/routes/route_service.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../modules/initial/pages/initial_page.dart';
+
+///
+/// define all of your application routes here!!
+///
+abstract class Routes {
+
+  static const INITIAL = '/initial';
+}
+
+///
+/// define all of your route services here!!
+///
+abstract class RouteServices {
+  const RouteServices._();
+
+  ///STARTING ROUTE OF APPLICATION
+  static const STARTING_POINT = Routes.INITIAL;
+
+  ///--> Add your application routes here!
+  static Map<String, Widget Function(BuildContext context)> routes = {
+    Routes.INITIAL: (context) => const HomePage(),
+  };
+
+  ///  add routing animation here!!
+  static PageRoute onGenerateRoute(settings, context) {
+    /// Custom Route Animations for Custom Pages
+    // if (settings.name == "/someRoute") {
+    //   return PageRouteBuilder(
+    //       settings: settings, // Pass this to make popUntil(), pushNamedAndRemoveUntil(), works
+    //       pageBuilder: (_, __, ___) => Routes.routes[settings.name]!(context),
+    //       transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c));
+    // }
+    return MaterialPageRoute(
+        builder: (_) => RouteServices.routes[settings.name]!(context));
+  }
+}
