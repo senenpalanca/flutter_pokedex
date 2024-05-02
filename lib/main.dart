94241a1debf884e560215561f8160c73536ca372
senenpalanca97 <senen@trialing.org>
2024-05-02 09:48:16 +0200

    Remove old Home widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,18 +37,3 @@ class Pokedex extends StatelessWidget {
     );
   }
 }
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Pokédex Code Challenge'),
-      ),
-      body: const Center(child: Text('Lee el README para comenzar')),
-    );
-  }
-}
