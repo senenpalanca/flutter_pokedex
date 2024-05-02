6a925b4ce2f829b70dd0bc5d157c7fe30c4ef8b8
senenpalanca97 <senen@trialing.org>
2024-05-02 10:46:15 +0200

    Update responsive breakpoints

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,8 @@ class Pokedex extends StatelessWidget {
       builder: (context, child) => ResponsiveBreakpoints.builder(
         child: child!,
         breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 0, end: 600, name: MOBILE),
+          const Breakpoint(start: 601, end: 800, name: TABLET),
           const Breakpoint(start: 801, end: 1920, name: DESKTOP),
           const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
         ],
